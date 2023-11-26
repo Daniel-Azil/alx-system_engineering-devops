7680459dd9351e42294b8c35faf65d51e026bbad
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-11-26 01:01:46 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# A CM that ensures the installation of Flask version
-# 2.1.0 using Puppet's package resource for pip3.
+# A CM that ensures the installation of Flask version 2.1.0 using Puppet's package resource for pip3.
 
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
