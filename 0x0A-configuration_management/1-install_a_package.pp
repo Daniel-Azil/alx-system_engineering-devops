620615fb82ed4d346c832b75d01608daa94bb6ba
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-11-26 01:07:50 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,13 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package {'python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
