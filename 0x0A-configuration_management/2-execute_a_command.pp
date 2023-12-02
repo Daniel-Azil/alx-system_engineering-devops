3d9cd95513adb4a6d20f42d02843f48931cfa1ee
Daniel Azil <danielazil.fse@gmail.com>
2023-12-02 21:50:33 +0000

    removed directories for upcoming projects in relation to cohort 16 deferral agreement

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Utilize Puppet's exec resource to create a manifest that
-# terminates a process named 'killmenow' using the pkill command.
-
-exec {'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell'
-}
