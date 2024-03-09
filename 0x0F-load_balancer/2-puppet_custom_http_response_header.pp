e93081e05040e01ab6bf74b203afffe9c4fe0bd4
Daniel Azil <danielazil.fse@gmail.com>
2024-03-09 18:02:01 +0000

    Modified puppet configuration to create http header response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,42 +1,8 @@
-package { 'nginx':
-  ensure => 'installed',
-}
-
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World',
-}
-
-file { '/var/www/html/404.html':
-  ensure  => 'file',
-  content => 'Ceci n\'est pas une page',
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-      add_header X-Served-By $::hostname;
-      root   /var/www/html;
-      index  index.html index.htm;
-
-      location /redirect_me {
-          return 301 http://cuberule.com/;
-      }
-
-      error_page 404 /404.html;
-      location /404 {
-        root /var/www/html;
-        internal;
-      }
-    }
-  ",
-}
-
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+# puppet configuration management that creates an http header response 
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
