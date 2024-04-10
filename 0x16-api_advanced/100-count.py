e8206712f3ad41a324b96a8070e846a80742b280
Daniel Azil <danielazil.fse@gmail.com>
2024-04-10 02:22:35 +0000

    Adhere to pycodestyle.

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,7 +6,7 @@ import requests
 
 
 def count_words(subreddit, word_list, after='', word_counter={}):
-    """ 
+    """
         that queries the Reddit API, parses the title of all hot articles,
         and prints a sorted count of given keywords (case-insensitive,
         delimited by spaces. Javascript should count as javascript, but
@@ -19,7 +19,8 @@ def count_words(subreddit, word_list, after='', word_counter={}):
                 word_counter[current_word.lower()] = 0
 
     if after is None:
-        sorted_words = sorted(word_counter.items(), key=lambda x: (-x[1], x[0]))
+        sorted_words = sorted(word_counter.items(),
+                              key=lambda x: (-x[1], x[0]))
         for word, count in sorted_words:
             if count:
                 print('{}: {}'.format(word, count))
@@ -28,7 +29,8 @@ def count_words(subreddit, word_list, after='', word_counter={}):
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     headers = {'user-agent': 'danazil1263 query'}
     params = {'limit': 100, 'after': after}
-    api_response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    api_response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
 
     if api_response.status_code != 200:
         return None
