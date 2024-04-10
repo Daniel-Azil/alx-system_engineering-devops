deefecc8a2cf6e196606fde45ec8b7f2a76f6224
Daniel Azil <danielazil.fse@gmail.com>
2024-04-10 01:39:54 +0000

    A module that queries the Reddit API and returns the number of subscribers     (not active users, total subscribers) for a given subreddit. If an invalid     subreddit is given, the function should return 0.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    A module that queries the Reddit API and prints the titles
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        A function that queries the reddit API and returns the number of subscribers
+    """
+
+    url = 'https://api.reddit.com/r/{}/about/.json'.format(subreddit)
+
+    user_agent = {'User-Agent': 'DanielAzilAdvancedAPI/1.0 (by /u/Daniel_Azil)'}
+
+    response = requests.get(url, headers=user_agent)
+
+    if response.status_code != 200:
+        print('None')
+    else:
+        top_ten = response.json()["data"]["children"]
+
+        limit = 0
+
+        for post in top_ten:
+            print(post["data"]["title"])
+            limit += 1
+            if limit == 10:
+                break
