f705848577f32c76344bf3d8cd9bfd7fba1f99bc
Daniel Azil <danielazil.fse@gmail.com>
2024-04-12 05:00:37 +0000

    Modified comment.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# fix 500 - Internal server error
+# fix internal server error 500 from apache.
 exec { '/var/www/html/wp-settings.wp':
   command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
